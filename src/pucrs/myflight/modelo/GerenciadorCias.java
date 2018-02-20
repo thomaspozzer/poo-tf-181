58174ddc8de18023624e08380cd3ff133295be84
Marcelo Cohen <mflashbr@gmail.com>
2018-02-20 19:05:08 -0300

    Construtor e método que retorna uma cópia da lista de todas as empresas

--- src/pucrs/myflight/modelo/GerenciadorCias.java
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -4,42 +4,15 @@ import java.util.ArrayList;
 
 public class GerenciadorCias {
 	private ArrayList<CiaAerea> empresas;
-	
-	public GerenciadorCias() {
-		empresas = new ArrayList<>();
-	}
-	
-	public void adicionar(CiaAerea nova) {
-		empresas.add(nova);
-	}
-	
-	public ArrayList<CiaAerea> listarTodas() {
-		ArrayList<CiaAerea> copia = new ArrayList<>();
-		for(CiaAerea cia: empresas)
-			copia.add(cia);
-		return copia;				
-	}
-	
-	public CiaAerea buscarCodigo(String cod) {
-		for(CiaAerea cia: empresas)
-			if(cod.equals(cia.getCodigo()))
-				return cia;
-		return null; // Não achei!
-	}
-	
-	public CiaAerea buscarNome(String nome) {
-		for(CiaAerea cia: empresas)
-			if(nome.equals(cia.getNome()))
-				return cia;
-		return null; // Não achei!
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+    public GerenciadorCias() {
+        this.empresas = new ArrayList<>();
+    }
+
+    public ArrayList<CiaAerea> listarTodas() {
+        ArrayList<CiaAerea> aux = new ArrayList<>();
+        for(int pos=0; pos < empresas.size(); pos++)
+            aux.add(empresas.get(pos));
+        return aux;
+    }
 }
