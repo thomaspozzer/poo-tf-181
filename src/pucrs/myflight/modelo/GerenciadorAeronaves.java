b485072a2dbb66bc8b0c20f8a13f84a9316d331f
Marcelo Cohen <marcelo.cohen@pucrs.br>
2018-04-09 22:36:35 -0300

    Ordenação usando recursos da interface Comparator

--- src/pucrs/myflight/modelo/GerenciadorAeronaves.java
+++ src/pucrs/myflight/modelo/GerenciadorAeronaves.java
@@ -2,6 +2,7 @@ package pucrs.myflight.modelo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class GerenciadorAeronaves {
 
@@ -27,9 +28,24 @@ public class GerenciadorAeronaves {
     }
 
     public void ordenarDescricao() {
+        // Usando Comparable<Aeronave> em Aeronave
         //Collections.sort(avioes);
-        avioes.sort( (Aeronave a1, Aeronave a2) ->
-            a1.getDescricao().compareTo(a2.getDescricao()));
+
+        // Usando expressão lambda
+        //avioes.sort( (Aeronave a1, Aeronave a2) ->
+        //    a1.getDescricao().compareTo(a2.getDescricao()));
+
+        // Mesma coisa, usando método static da interface Comparator:
+        //avioes.sort(Comparator.comparing(a -> a.getDescricao()));
+
+        // Invertendo o critério de comparação com reversed():
+        avioes.sort(Comparator.comparing(Aeronave::getDescricao).reversed());
+    }
+
+    public void ordenarCodigoDescricao() {
+       // Ordenando pelo código e desempatando pela descrição
+       avioes.sort(Comparator.comparing(Aeronave::getCodigo).
+               thenComparing(Aeronave::getDescricao));
     }
 
     public void ordenarCodigo() {
